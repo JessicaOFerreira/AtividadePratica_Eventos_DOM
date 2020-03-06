46c757446fffb7a89309b8ad27ac36a589608efc
JessicaOFerreira <51684387+JessicaOFerreira@users.noreply.github.com>
2020-03-06 14:18:39 -0300

    Correção Validação

--- js/listener.js
+++ js/listener.js
@@ -2,18 +2,25 @@ let textoNovo = document.querySelector("#txt");
 let lista = document.querySelector(".lista");
 let lista2 = document.querySelector(".lista2");
 let botao = document.querySelector("#btn");
+let validation = document.createElement("span");
+validation.id="valid";
 let quebra= document.createElement("br");
 
 botao.addEventListener("click", function (evento) {
     evento.preventDefault();
 
     if (textoNovo.value == "" || textoNovo.value == " ") {
-        let validation = document.createElement("span");
-        let textoSp = document.createTextNode("Erro. Campo Obrigatorio!");
+ 
+        let textoSp = document.createTextNode("*Erro. Campo Obrigatorio!");
 
         validation.append(textoSp);
         botao.after(quebra);
-        botao.after(validation);
+        quebra.after(validation);
+
+        textoNovo.addEventListener("focus", function(){
+            validation.innerHTML="";
+        });
+
     }
 
     else{
